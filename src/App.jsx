556a3a1bafd50673adec61d5434fd99f3cfe6d32
kalyan017 <kalyanreddy1719@gmail.com>
2026-06-22 15:13:11 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div>
       <h1>SELF INTRODUCTION</h1>
-      <Selfintro/>
+      <Selfintro />
     </div>
   )
 }
--- src/Selfintro.jsx
+++ src/Selfintro.jsx
@@ -8,10 +8,9 @@ export default function Selfintro() {
                 <li><p>I have a strong interest in software development and web technologies. My technical skills include Python, SQL, HTML, CSS, Javascript, MySQL, Git, and GitHub. I also have a good understanding of Object-Oriented Programming, RDBMS, and the Software Development Life Cycle.</p></li>
                 <li> <p>During my internships, I worked as a Data Science Intern at Avishkar Tech Solutions, where I analyzed and cleaned large datasets and automated data preprocessing tasks. I also worked on Azure cloud technologies, building ETL pipelines and optimizing SQL queries for better performance.</p></li>
                 <li><p>I have developed projects such as a Travelling Management System, which helps users plan trips with personalized recommendations, and a Driver Drowsiness Monitoring System using OpenCV and Python to improve road safety through real-time monitoring and alerts.</p></li>
-                <li> <p>I have developed projects such as a Travelling Management System, which helps users plan trips with personalized recommendations, and a Driver Drowsiness Monitoring System using OpenCV and Python to improve road safety through real-time monitoring and alerts.</p></li>
                 <li> <p>My strengths include problem-solving, analytical thinking, adaptability, teamwork, and a willingness to learn new technologies quickly.</p></li>
                 <li> <p>My career goal is to build a successful career in the IT industry where I can continuously improve my technical skills, gain hands-on experience, and contribute to innovative software solutions while growing professionally.</p></li></ul>
-                <p>Thank you.</p>
+            <p>Thank you.</p>
         </div>
     )
 }
