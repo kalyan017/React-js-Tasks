9cb8f233210b01b2e6bc5678a599cca779f95b77
kalyan017 <kalyanreddy1719@gmail.com>
2026-06-22 15:10:13 +0530

    Completed Task-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import "./App.css"
+import Selfintro from './Selfintro'
 
 export default function App() {
   return (
     <div>
-      <h1>Self Introduction</h1>
+      <h1>SELF INTRODUCTION</h1>
+      <Selfintro/>
     </div>
   )
 }
