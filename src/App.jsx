4d77a25e8af8a9c2d5bd388813428ec765081f48
kalyan017 <kalyanreddy1719@gmail.com>
2026-06-23 15:43:09 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./App.css"
-import child from './child'
+import Selfintro from './Selfintro'
 
 export default function App() {
   return (
     <div>
-      <h1>Task-2</h1>
-      <child/>
+      <h1>SELF INTRODUCTION</h1>
+      <Selfintro />
     </div>
   )
 }
--- src/child.jsx
+++ src/child.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function child() {
+export default function Child({ name, age, city, phonenum }) {
   return (
     <div>
-      
+      <h1>Name:{name}</h1>
+      <h1>Age:{age}</h1>
+      <h1>City:{city}</h1>
+      <h1>Phone Num:{phonenum}</h1>
     </div>
   )
 }
