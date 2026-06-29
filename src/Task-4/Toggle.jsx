3e5e6e39a62329453288a08ecf00be278544e9f1
kalyan017 <kalyanreddy1719@gmail.com>
2026-06-29 15:42:45 +0530

    Completed

--- src/Task-4/Toggle.jsx
+++ src/Task-4/Toggle.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Toggle() {
+    const [theme,SetTheme]=useState(false);
   return (
-    <div>
-      
+    <div style={{
+        backgroundColor:theme?"#100000":"white",
+        color:theme?"white":"black",
+        padding:"30px"
+        }}>
+      <h2>Toggle Theme</h2>
+      <button onClick={()=>SetTheme(!theme)}>{theme?"Dark":"Light"} Mode</button>
+      <p>You are in {theme?"Dark":"Light"} Mode</p>
     </div>
   )
 }
