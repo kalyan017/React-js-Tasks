f033523e6af9cb5cf4cf842797f6b429ce61f73a
kalyan017 <kalyanreddy1719@gmail.com>
2026-07-01 14:47:50 +0530

    crrated

--- src/Task-6/Api.jsx
+++ src/Task-6/Api.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import UseEffectApi from './UseEffectApi'
 
 export default function Api() {
   return (
     <div>
-      
+      <UseEffectApi />
     </div>
   )
 }
---
+++ src/Task-6/UseEffectApi.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+export default function UseEffectApi() {
+    const [user, setUser] = useState([])
+    const [loading, setLoading] = useState(true)
+    return (
+        <div>
+
+        </div>
+    )
+}
