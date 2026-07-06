66d5b648677c70439c9564a1c88599cd7a3cab23
kalyan017 <kalyanreddy1719@gmail.com>
2026-07-06 14:31:10 +0530

    created a context

--- src/main.jsx
+++ src/main.jsx
@@ -11,9 +11,10 @@ import RegistrationForm from './Task-5/RegistrationForm.jsx'
 import Api from './Task-6/Api.jsx'
 import UseRefHook from './Task-7/UseRefHook.jsx'
 import Application from './Task-8/Application.jsx'
+import UseContextHook from './Task-9/UseContextHook.jsx'
 
 createRoot(document.getElementById('root')).render(
   
-    <Application />
+    < UseContextHook/>
   
 )
