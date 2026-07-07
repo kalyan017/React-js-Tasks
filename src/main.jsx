23dfdc9dca539a2d231e5ad4007e4c41e0f4daf2
kalyan017 <kalyanreddy1719@gmail.com>
2026-07-07 20:43:40 +0530

    created basic files

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,12 @@ import Api from './Task-6/Api.jsx'
 import UseRefHook from './Task-7/UseRefHook.jsx'
 import Application from './Task-8/Application.jsx'
 import UseContextHook from './Task-9/UseContextHook.jsx'
+import Routing from './Task-10/routing.jsx'
 
 createRoot(document.getElementById('root')).render(
-  
-    < UseContextHook/>
+  <StrictMode>
+    <Routing/>
+  </StrictMode>
+   
   
 )
