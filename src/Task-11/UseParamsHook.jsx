0508050d5fdc49788d1076d1a60ba398982bc180
kalyan017 <kalyanreddy1719@gmail.com>
2026-07-08 14:54:26 +0530

    created  necessary files

--- src/Task-11/UseParamsHook.jsx
+++ src/Task-11/UseParamsHook.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import UserList from './UserList'
 
 export default function UseParamsHook() {
   return (
     <div>
-      
+      <UserList/>
     </div>
   )
 }
---
+++ src/Task-11/UserList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserList() {
+  return (
+    <div>
+      <h2>kkk</h2>
+    </div>
+  )
+}
