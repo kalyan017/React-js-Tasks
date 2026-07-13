480ad8c86f869760c4faefafec23820135beb1bb
kalyan017 <kalyanreddy1719@gmail.com>
2026-07-13 21:16:55 +0530

    crrated

--- src/Task-13/UseSearchParam.jsx
+++ src/Task-13/UseSearchParam.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import PageInation from './PageInation'
 
 export default function UseSearchParam() {
   return (
     <div>
-      
+      <PageInation/>
     </div>
   )
 }
