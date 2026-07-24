78137bd4b79c8876e070f81c32015aee0705a306
kalyan017 <kalyanreddy1719@gmail.com>
2026-07-24 21:37:03 +0530

    modified

--- src/Task-10/Contact.jsx
+++ src/Task-10/Contact.jsx
@@ -5,7 +5,6 @@ export default function Contact() {
     return (
         <div className="page">
             <h1>Contact Me</h1>
-
             <form className="contact">
                 <input
                     type="text"
